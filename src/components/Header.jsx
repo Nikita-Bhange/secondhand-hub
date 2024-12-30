a166a27491c213e782725f435216f9735ff8de6f
shaikh-ayman <aymanshaikh2023@gmail.com>
2024-12-30 22:40:14 +0530

    Added Login,Signup and Sell

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const style = "text-3xl, cursor-pointer, ml-[25px] mobile:ml-[5px]";
   return (
     <>
-      <div className="sticky top-0 z-50 bg-white shadow-md ">
+      <div className="sticky top-0 w-full z-50 bg-white shadow-md ">
         <div className="wrapper pr-[10px] flex justify-between items-center mobile:pl-0 mobile:pr-0">
           <div className="w-[160px] m-3 pl-4">
             <NavLink to="/">
@@ -19,7 +19,7 @@ const Header = () => {
             </NavLink>
           </div>
 
-          <div className="inline-flex items-center justify-center border border-gray-400 px-5  py-2 my-5 mx-3 rounded-3xl w-2/4 sm:w-1/4 ">
+          <div className="inline-flex items-center justify-center border border-gray-400 px-5  py-2 my-5 mx-3 rounded-3xl w-2/3 sm:w-1/4 ">
             <input
               className="flex-1 outline-none bg-inherit text-sm "
               type="text"
@@ -29,20 +29,16 @@ const Header = () => {
           </div>
 
           <div className="flex pt-3">
-            <div className="hidden sm:flex text-xl pt-2 mr-3 font-bold text-blue-900">
-              <NavLink className={style}>
-                <p>Sell</p>
-              </NavLink>
-            </div>
-            <div className="hidden sm:flex text-xl mr-3  font-bold ">
-              <NavLink className={style}>
-                <button className="bg-blue-900 rounded-lg text-white p-3">
-                  Shop
-                </button>
-              </NavLink>
-            </div>
+            
+          <ul className='hidden sm:flex gap-2 text-m pt-2  font-bold text-black'>
+         <NavLink to='/home' className={style}><p>Home</p></NavLink>
+         <NavLink className={style}><p>Categories</p></NavLink>
+         <NavLink className={style}><p>About us</p></NavLink>
+         
+        
+       </ul>
 
-            <div className="hidden sm:flex mr-3 pt-2 font-bold text-black">
+            <div className="hidden sm:flex mr-3 ml-3 pt-2 font-bold text-black">
               <NavLink className={style} to="/addtocart">
                 <ShoppingCartOutlined />
               </NavLink>
@@ -51,6 +47,11 @@ const Header = () => {
             <div className="hidden sm:flex mr-3 pt-2 font-bold text-black">
               <NavLink className={style} to="/profile">
                 <AccountCircle />
+              </NavLink>
+            </div>
+            <div className="hidden sm:flex text-xl pt-2 ml-3 font-bold text-blue-950 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-xl w-20 h-11 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105">
+              <NavLink className={style} to={"/sell"}>
+                <p>Sell</p>
               </NavLink>
             </div>
           </div>
